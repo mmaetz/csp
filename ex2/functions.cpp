2de9fc57c01766f681bb8c5680ed9987b66081e3
Marc Maetz <marc.maetz@gmail.com>
2015-01-13 19:20:06 +0100

    init ex2 with code from ex1

---
+++ ex2/functions.cpp
@@ -0,0 +1,145 @@
+#include <vector>
+#include <random>
+#include "functions.h"
+#include "defn.h"
+#include <iostream>
+#include <cmath>
+
+void initalize(std::vector< std::vector< std::vector<char> > >& cluster, const int seed, const double p)
+{
+	int icounter;
+	int jcounter;
+	int kcounter;
+
+	std::mt19937 gen(seed);
+	std::uniform_real_distribution<double> dis(0,1);
+
+  for (icounter=0; icounter<N; icounter++)
+	{
+		for (jcounter=0; jcounter<N; jcounter++) 
+		{
+			for (kcounter=0; kcounter<N; kcounter++) 
+			{
+				if(dis(gen) < p)
+				{
+					cluster[icounter][jcounter][kcounter]=1;
+				}
+				else
+					cluster[icounter][jcounter][kcounter]=-1;
+			}
+		}
+	}
+}
+
+int energy_diff(std::vector< std::vector< std::vector<char> > >& cluster, int i, int j, int k, int J)
+{
+	double energy = 0, energy1 = 0, energy2 = 0;
+	energy1 += J*cluster[i][j][k]*cluster[period(i+1)][j][k];
+	energy1 += J*cluster[i][j][k]*cluster[period(i-1)][j][k];
+	energy1 += J*cluster[i][j][k]*cluster[i][period(j-1)][k];
+	energy1 += J*cluster[i][j][k]*cluster[i][period(j+1)][k];
+	energy1 += J*cluster[i][j][k]*cluster[i][j][period(k+1)];
+	energy1 += J*cluster[i][j][k]*cluster[i][j][period(k-1)];
+
+	energy2 -= J*cluster[i][j][k]*cluster[period(i+1)][j][k];
+	energy2 -= J*cluster[i][j][k]*cluster[period(i-1)][j][k];
+	energy2 -= J*cluster[i][j][k]*cluster[i][period(j-1)][k];
+	energy2 -= J*cluster[i][j][k]*cluster[i][period(j+1)][k];
+	energy2 -= J*cluster[i][j][k]*cluster[i][j][period(k+1)];
+	energy2 -= J*cluster[i][j][k]*cluster[i][j][period(k-1)];
+
+	energy = energy1-energy2;
+	return energy;
+}
+
+void flip(std::vector< std::vector< std::vector<char> > >& cluster, int i, int j, int k)
+{
+	cluster[i][j][k] = -cluster[i][j][k];
+}
+
+int energy_tot(std::vector< std::vector< std::vector<char> > >& cluster, int J)
+{
+	int i, j, k;
+	double energy;
+
+	std::vector< std::vector< std::vector<bool> > > visited(N, std::vector< std::vector<bool> >(N, std::vector<bool>(N)));
+
+  for (i=0; i<N; i++)
+	{
+		for (j=0; j<N; j++) 
+		{
+			for (k=0; k<N; k++) 
+			{
+				visited[i][j][k] = 1;
+//				if(!visited[i][j][k])
+//					cluster[i][j][k]=1;
+				if(!visited[period(i+1)][j][k]);
+					energy -= J*cluster[i][j][k]*cluster[period(i+1)][j][k];
+				if(!visited[period(i-1)][j][k]);
+					energy -= J*cluster[i][j][k]*cluster[period(i-1)][j][k];
+				if(!visited[i][period(j-1)][k]);
+					energy -= J*cluster[i][j][k]*cluster[i][period(j-1)][k];
+				if(!visited[i][period(j+1)][k]);
+					energy -= J*cluster[i][j][k]*cluster[i][period(j+1)][k];
+				if(!visited[i][j][period(k+1)]);
+					energy -= J*cluster[i][j][k]*cluster[i][j][period(k+1)];
+				if(!visited[i][j][period(k-1)]);
+					energy -= J*cluster[i][j][k]*cluster[i][j][period(k-1)];
+			}
+		}
+	}
+
+	return energy;
+}
+
+double magn_tot(std::vector< std::vector< std::vector<char> > >& cluster)
+{
+	int i, j, k;
+	double magnetization = 0;
+  for (i=0; i<N; i++)
+	{
+		for (j=0; j<N; j++) 
+		{
+			for (k=0; k<N; k++) 
+			{
+					magnetization += cluster[i][j][k];
+			}
+		}
+	}
+	return magnetization/double(N);
+}
+
+
+int period(int index)
+{
+	switch (index) {
+		case -1: 
+			return N-1;
+		case N:
+			return 0;
+		default:
+			return index;
+	}
+}
+
+const std::vector<double> prob_list(double beta)
+{
+	std::vector<double> list;
+//	list.push_back(-exp(4.0/beta));
+	list.push_back( std::min(double(1), exp(-4.0/beta)) );
+	list.push_back( std::min(double(1), exp(-8.0/beta)) );
+	list.push_back( std::min(double(1), exp(-12.0/beta)) );
+	return list;
+}
+
+double metropolis_prob(int energy_diff, const std::vector<double>& list)
+{
+	switch (energy_diff) {
+		case 4:
+			return list[0];
+		case 8:
+			return list[1];
+		case 12:
+			return list[2];
+	}
+}
---
+++ ex2/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "latticeview.h"
+#include "functions.h"
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <cmath>
+
+main () {
+
+	int icounter;
+	int jcounter;
+
+//	double kb = 8.6173324e-5;
+//	double T = 300;
+	double beta = 5;
+
+	const int seed_occ = 1;
+	const int seed_pos = 2;
+	const int seed_en = 3;
+	const double p = 0.5;
+
+	std::mt19937 gen_pos(seed_pos);
+	std::mt19937 gen_en(seed_en);
+	std::uniform_real_distribution<double> en(0,1);
+	std::uniform_int_distribution<> pos(0,N-1);
+
+	std::vector< std::vector< std::vector<char> > > cluster(N, std::vector< std::vector<char> >(N, std::vector<char>(N)));
+
+
+	initalize(cluster, seed_occ, p);
+	Print_lattice (cluster[0], N, N, ImageWidth, ImageHeight, "random1.ppm");
+	
+	const int steps = pow(10, 7);
+	int i,j,k;
+
+////	 magnetism 1 or -1
+	int J = 1;
+
+	double etot = energy_tot(cluster, J);
+	std::cout << "Total energy: " << etot << std::endl;
+	double mtot = magn_tot(cluster);
+	std::cout << "Total magnetization: " << mtot << std::endl;
+	double this_energy;
+
+	const std::vector<double> metro_prob = prob_list(beta);
+
+	for(icounter = 0; icounter < steps; icounter++)
+	{
+		i = pos(gen_pos);
+		j = pos(gen_pos);
+		k = pos(gen_pos);
+		this_energy = energy_diff(cluster, i, j, k, J);
+		if( this_energy <= 0 )
+			flip(cluster, i, j, k);
+		else
+		{
+//			if( std::min(double(1),exp(-double(this_energy)/beta)) >= en(gen_en))
+			if( metropolis_prob(this_energy, metro_prob) >= en(gen_en))
+			{
+				flip(cluster, i, j, k);
+			}
+		}
+
+	}
+	Print_lattice (cluster[0], N, N, ImageWidth, ImageHeight, "random2.ppm");
+	etot = energy_tot(cluster, J);
+	std::cout << "Total energy: " << etot << std::endl;
+	mtot = magn_tot(cluster);
+	std::cout << "Total magnetization: " << mtot << std::endl;
+}
